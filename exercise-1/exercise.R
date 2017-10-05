c08c63bd4d1236bc6f3eee97d836fc36c48cc707
Larissa Ho <holarissa12@gmail.com>
2017-10-05 14:34:45 -0700

    Add R variables

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -22,4 +22,4 @@ total.cost <- puppies * puppy.price
 too.expensive <- total.cost > 1000
 
 # Create a variable `max.puppies`, which is the nuber of puppies you can afford for $1K.
-max.puppies <- total.cost/1000
+max.puppies <- 1000/puppy.price
